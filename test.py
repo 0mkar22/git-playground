3d68e2a4ed24ab78f4bdb9736b57ad38a8e67222
Omkar Bhogte <obhogate48@gmail.com>
2026-04-14 16:08:07 +0530

    sdafagas

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ def add(a, b):
 assert add(2, 3) == 5, "Should be 5"
 assert add(-1, 1) == 0, "Should be 0"
 
-print("All simple assertions passed! asad asdasd")
+print("All simple assertions passed! asad ")
 
