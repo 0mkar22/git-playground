92f7673a57ed8c068e4c28812f4aef55dd2b15cb
Omkar Bhogte <obhogate48@gmail.com>
2026-04-14 18:14:32 +0530

    testing adding new line

---
+++ test.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+# Basic manual tests
+assert add(2, 3) == 5, "Should be 5"
+assert add(-1, 1) == 0, "Should be 0"
+
+print("All simple assertions passed! ")
+
+print("Running more tests...")
+
+print("All tests completed successfully!")
